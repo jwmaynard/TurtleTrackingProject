41396edca81de109fbc4904921e4c261c331c644
jwmaynard <jwmaynard12@gmail.com>
2024-10-02 12:07:11 -0400

    Revert "Convert to while loop"
    This reverts commit bbd6c748b7baebe8ceef38c84c126c7b9bab5c0c.
    
    revert while loop

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -16,12 +16,14 @@ file_name = 'data/raw/Sara.txt'
 file_object = open(file_name,'r')
 
 #Read contents of file into a list
-lineString = file_object.readline()
+line_list = file_object.readlines()
+
+#Close the file
+file_object.close()
 
 #Pretend we read one line of data from the file
-while lineString:
+for lineString in line_list:
     if lineString[0] in ("#", "u"):
-        lineString = file_object.readline()
         continue
 
     #Split the string into a list of data items
@@ -36,7 +38,4 @@ while lineString:
 
     #Print the location of sara
     print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
-
-    #read next line
-    lineString = file_object.readline()
 
