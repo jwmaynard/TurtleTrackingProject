ee4b263633a0cd044cad296ecf8b84a3e469da94
jwmaynard <jwmaynard12@gmail.com>
2024-10-07 11:27:27 -0400

    Finish dictionary loop

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -10,7 +10,7 @@
 #--------------------------------------------------------------
 
 #Ask user for a date
-user_date = '7/3/2003' #input("Enter a date: ")
+user_date = input("Enter a date: ")
 
 #Create a variable pointing to the data file
 file_name = 'data/raw/Sara.txt'
@@ -49,10 +49,17 @@ for lineString in line_list:
         date_dict[record_id] = obs_date
         location_dict[record_id] = (obs_lat, obs_lon)
 
-    #initialize key list
-    keys = []
+#initialize key list
+keys = []
 
-    #Loop through items in date_dict
-    for key, value in date_dict.items():
+#Loop through items in date_dict
+for key, value in date_dict.items():
+    if value == user_date:
+        keys.append(key)
     
 
+#loop through keys and report location
+for key in keys:
+    lat = location_dict[key][0]
+    lng = location_dict[key][1]
+    print(f"On the {user_date}, Sara the turtle was seen at {lat}d Lat, {lng}d Longitude")
